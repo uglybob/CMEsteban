0d5d6c52712268fc25c2de23a490167f1ba1396b
Sebastian Reinhold <sebastian@bitbernd.de>
2015-06-02 11:17:03 +0200

    cleanup

--- Bh/Page/EditForm.php
+++ Bh/Page/EditForm.php
@@ -11,32 +11,28 @@ class EditForm
     {
         $this->controller = $controller;
         $this->logic = $controller->getLogic();
-
         $this->id = $id;
-        // @todo hard coded
         $this->class = $class;
-        $editString = $this->controller->getClass('Page', 'Edit' . $this->class);
 
         $this->form = new htmlform('edit' . $this->class, ['label' => 'speichern']);
-        $object = $this->logic->{'get' . $this->class}($this->id);
+        $this->object = $this->logic->{'get' . $this->class}($this->id);
 
-        $this->createForm($object);
+        $this->create();
 
-        if ($object) {
+        if ($this->object) {
             $this->title = $this->class . ' editieren';
-            $this->populateForm($object);
+            $this->populate();
         } else {
             $this->title = $this->class . ' erstellen';
-            // @todo hard coded
-            $classString = '\Bh\Content\Entity\\' . $this->class;
-            $object = new $classString();
+            $classString = $this->controller->getClass('Entity', $this->class);
+            $this->object = new $classString();
         }
 
         $this->form->process();
 
         if ($this->form->validate()) {
-            $this->saveForm($object);
-            $this->logic->save($object);
+            $this->save();
+            $this->logic->save($this->object);
             $this->logic->commit();
             $this->form->clearSession();
             Page::redirect('/list/' . lcfirst($this->class));
