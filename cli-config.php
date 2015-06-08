223ffded682620c5356ed4c28d92354fa3553202
Sebastian Reinhold <sebastian@bitbernd.de>
2015-06-08 18:46:24 +0200

    fixed doctrine cli

--- cli-config.php
+++ cli-config.php
@@ -2,7 +2,7 @@
 require_once 'vendor/autoload.php';
 
 $controller = new \Bh\Lib\Controller();
-$entityManager = $controller->getMapper();
+$entityManager = $controller->getMapper()->getEntityManager();
 
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
 
