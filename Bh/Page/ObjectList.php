3553d5b245a564fbc44208a65901fc31904ea404
Sebastian Reinhold <sebastian@bitbernd.de>
2015-08-27 22:55:31 +0100

    adapt object list

--- Bh/Page/ObjectList.php
+++ Bh/Page/ObjectList.php
@@ -4,44 +4,51 @@ namespace Bh\Page;
 
 use Bh\Lib\Html;
 
-class ObjectList extends Backend
+class ObjectList extends Page
 {
-    // {{{ constructor
-    public function __construct($controller, $path)
+    // {{{ renderList
+    protected function renderList(array $objects, $edit = false)
     {
-        $this->class = $path[1];
-        parent::__construct($controller);
-        $this->title = $this->class . ' list';
-    }
-    // }}}
-
-    // {{{ renderContent
-    protected function renderContent()
-    {
-        $content = '';
-        $objects = [];
-
-        $getter = 'get' . ucfirst($this->class) . 's';
-        $objects = $this->controller->$getter();
+        $list = '';
 
         foreach ($objects as $object) {
-            $content .= HTML::div('',
+            $properties = $this->loadProperties($object);
+
+            $propertyList = '';
+            foreach ($properties as $property) {
+                $propertyList .= HTML::span('', $property);
+            }
+
+            if ($edit) {
+               $list .= HTML::div('',
+                    HTML::span('',
+                        HTML::a('href=/edit/' . $edit . '/' . $object->getId(), $propertyList)
+                    ) .
+                    HTML::span('',
+                        HTML::a('href=/delete/' . $edit . '/' . $object->getId(), 'x')
+                    )
+                );
+            } else {
+               $list .= HTML::div('',
+                    HTML::span('', $propertyList)
+                );
+            }
+        }
+
+        if ($edit) {
+            $list .= HTML::div('',
                 HTML::span('',
-                    HTML::a('href=/edit/' . $this->class . '/' . $object->getId(), $object->getName())
-                ) .
-                HTML::span('',
-                    HTML::a('href=/delete/' . $this->class . '/' . $object->getId(), 'löschen')
+                    HTML::a('href=/edit/' . $edit . '/', $edit . ' hinzufügen')
                 )
             );
         }
-
-        $content .= HTML::div('',
-            HTML::span('',
-                HTML::a('href=/edit/' . $this->class . '/', $this->class . ' hinzufügen')
-            )
-        );
-
-        return $content;
+        return $list;
+    }
+    // }}}
+    // {{{ loadProperties
+    protected function loadProperties($object)
+    {
+        return [$object->getName()];
     }
     // }}}
 }
