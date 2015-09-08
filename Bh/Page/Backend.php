4decbca50c1940bab2b1cb331c3b67450b7c8c90
Sebastian Reinhold <sebastian@bitbernd.de>
2015-09-08 19:59:11 +0100

    cleanup

--- Bh/Page/Backend.php
+++ Bh/Page/Backend.php
@@ -4,11 +4,4 @@ namespace Bh\Page;
 
 abstract class Backend extends Page {
     protected $accessLevel = 1;
-
-    // {{{ constructor
-    public function __construct($controller)
-    {
-        parent::__construct($controller);
-    }
-    // }}}
 }
