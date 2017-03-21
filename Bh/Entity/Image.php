537ceca636470025ec5a6ed169a8c06e6efb6aae
Sebastian Reinhold <sebastian@bitbernd.de>
2017-03-21 15:51:59 +0100

    add image functionality

--- Bh/Entity/Image.php
+++ Bh/Entity/Image.php
@@ -2,15 +2,51 @@
 
 namespace Bh\Entity;
 
+use Bh\Page\Module\HTML;
+
 class Image extends Named
 {
     protected $alt;
-    protected $path;
+    protected $src;
+    protected $private;
 
     // {{{ constructor
-    public function __construct($name)
+    public function __construct($name, $private = false, $src = null, $alt = null)
     {
         parent::__construct($name);
+
+        $this->private = $private;
+        $this->src = $src;
+        $this->alt = $alt;
+    }
+    // }}}
+
+    // {{{ toString
+    public function __toString()
+    {
+        if ($this->private) {
+            try {
+                //$image = imagecreatefromjpeg($this->src);
+                //header('Content-Type: image/jpeg');
+                //$src = imagejpeg($image);
+            } catch (\Exception $e) {
+                $src = '';
+            }
+        } else {
+            $src = $this->src;
+        }
+
+        $attributes = ['src' => $src];
+
+        if ($this->alt) {
+            $attributes['alt'] = $this->alt;
+        }
+
+        try {
+            $image = HTML::img($attributes);
+        } catch (\Exception $e) {
+            $image = '';
+        }
     }
     // }}}
 }
