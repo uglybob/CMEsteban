6dc6735fd234f2de72ee8d74c436b33c3c28a4e5
Sebastian Reinhold <sebastian@bitbernd.de>
2017-03-23 21:37:33 +0100

    redirect to actual link on 0 access level

--- Bh/Page/Image.php
+++ Bh/Page/Image.php
@@ -12,11 +12,16 @@ class Image extends Page
     {
         $object = $this->controller->getImage($this->getPath(1));
 
+        if ($object->getLevel() == 0) {
+            $this->redirect($object->getSrc());
+        }
+
         $path = $object->getSrc();
         $path = ltrim($path, '/');
         $image = imagecreatefromjpeg($path);
 
         header('Content-Type: image/jpeg');
+
         return imagejpeg($image);
     }
     // }}}
