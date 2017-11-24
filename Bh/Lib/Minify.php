684894801936574e060863c9c49c6d058d92ad2a
Sebastian Reinhold <sebastian@bitbernd.de>
2017-11-24 19:40:44 +0100

    cleanup

--- Bh/Lib/Minify.php
+++ Bh/Lib/Minify.php
@@ -2,18 +2,14 @@
 
 namespace Bh\Lib;
 
-class Minify
+abstract class Minify
 {
     public static function minify($type, $files) {
         $index = md5(implode(' ', $files)) . ".$type";
         $cached = Cache::get($index);
 
         if (!$cached) {
-            if ($type == 'css') {
-                $minifier = new \MatthiasMullie\Minify\CSS();
-            } else {
-                $minifier = new \MatthiasMullie\Minify\JS();
-            }
+            $minifier = ($type == 'css') ? new \MatthiasMullie\Minify\CSS() : new \MatthiasMullie\Minify\JS();
 
             foreach ($files as $file) {
                 $minifier->add(Setup::getSettings('Path') . $file);
