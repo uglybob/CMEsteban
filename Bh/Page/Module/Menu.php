3d7356887dc319c033e726af4d4f7dcf307c8cac
Sebastian Reinhold <sebastian@bitbernd.de>
2017-12-02 21:16:27 +0100

    implement menu class

---
+++ Bh/Page/Module/Menu.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Bh\Page\Module;
+
+class Menu
+{
+    // {{{ constructor
+    public function __construct($links = [])
+    {
+        $this->links = $links;
+    }
+    // }}}
+    // {{{ toString
+    public function __toString()
+    {
+        $menu = '';
+
+        foreach ($this->links as $title => $link) {
+            $menu .= HTML::a(['href' => $link], $title);
+        }
+
+        return Html::div(['class' => 'menu'], $menu);
+    }
+    // }}}
+}
