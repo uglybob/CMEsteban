9855c25a2cb9a636fe7d04b46cfe3721e3e9d5a6
Sebastian Reinhold <sebastian@bitbernd.de>
2017-12-16 17:36:46 +0100

    add clean links tests

--- Bh/Test/CleanLinksTest.php
+++ Bh/Test/CleanLinksTest.php
@@ -28,4 +28,40 @@ class CleanLinksTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals('<a href="https://www.url.com">>url.com</a>', Page::cleanLinks('www.url.com'));
     }
     // }}}
+    // {{{ testUrlHttp
+    public function testUrlHttp()
+    {
+        $this->assertEquals('<a href="http://www.url.com">>url.com</a>', Page::cleanLinks('http://www.url.com'));
+    }
+    // }}}
+    // {{{ testUrlHttps
+    public function testUrlHttps()
+    {
+        $this->assertEquals('<a href="https://www.url.com">>url.com</a>', Page::cleanLinks('https://www.url.com'));
+    }
+    // }}}
+    // {{{ testUrlMultiple
+    public function testUrlMultiple()
+    {
+        $this->assertEquals('<a href="https://www.url.com">>url.com</a> <a href="https://www.url2.com">>url2.com</a>', Page::cleanLinks('www.url.com www.url2.com'));
+    }
+    // }}}
+    // {{{ testUrlRecursive
+    public function testUrlRecursive()
+    {
+        $this->assertEquals('<a href="https://www.url.com">>url.com</a> <a href="https://www.url.co">>url.co</a>', Page::cleanLinks('www.url.com www.url.co'));
+    }
+    // }}}
+    // {{{ testUrlEnclosed
+    public function testUrlEnclosed()
+    {
+        $this->assertEquals('|<a href="https://www.url.com">>url.com</a>|', Page::cleanLinks('|www.url.com|'));
+    }
+    // }}}
+    // {{{ testUrlKnownSite
+    public function testUrlKnownSite()
+    {
+        $this->assertEquals('<a href="https://www.twitter.com/account">>twitter</a>', Page::cleanLinks('www.twitter.com/account'));
+    }
+    // }}}
 }
