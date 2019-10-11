8aaa76720b64d3be6ea6bbca142f64669c832db3
Sebastian Reinhold <sebastian@bitbernd.de>
2019-10-12 00:17:04 +0200

    implement table tests

--- CMEsteban/Page/Module/Table.php
+++ CMEsteban/Page/Module/Table.php
@@ -5,10 +5,20 @@ namespace CMEsteban\Page\Module;
 class Table
 {
     // {{{ constructor
-    public function __construct($page, array $items, array $attributes)
+    public function __construct($page, array $items, array $attributes = [])
     {
         $page->addStylesheet('/CMEsteban/Page/css/table.css');
         $this->list = '';
+
+        if (empty($attributes)) {
+            $key = array_key_first($items);
+
+            if (isset($items[$key])) {
+                foreach ($items[$key] as $attribute => $value) {
+                    $attributes[$attribute] = $attribute;
+                }
+            }
+        }
 
         $header = '';
         foreach ($attributes as $attribute => $caption) {
@@ -17,9 +27,13 @@ class Table
         $this->list .= HTML::div(['.ctheader'], HTML::div(['.ctrow'], $header));
 
         foreach ($items as $item) {
+            $propertyList = '';
+
             foreach ($attributes as $attribute => $caption) {
-                $propertyList .= HTML::div([".$caption", '.ctcell'], $item[$attribute]);
+                $class = preg_replace('/[^_\-a-z0-9]/i', '_', $caption);
+                $propertyList .= HTML::div([".$class", '.ctcell'], $item[$attribute]);
             }
+
             $this->list .= HTML::div(['.ctrow'], $propertyList);
         }
 
---
+++ CMEsteban/Test/TableTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace CMEsteban\Test;
+
+use CMEsteban\Page\Module\Table;
+
+class TableTest extends \PHPUnit\Framework\TestCase
+{
+    // {{{ setUp
+    protected function setUp() : void
+    {
+        $this->controller = new \CMEsteban\Lib\Controller();
+        $this->page = new PageTestClass($this->controller);
+ 
+        parent::setUp();
+    }
+    // }}}
+
+    // {{{ testEmpty
+    public function testEmpty()
+    {
+        $table = new Table($this->page, []);
+
+        $this->assertEquals('<div class="ctable"><div class="ctheader"><div class="ctrow"></div></div></div>', $table->__toString());
+    }
+    // }}}
+    // {{{ testSimple
+    public function testSimple()
+    {
+        $table = new Table($this->page, [
+            ['attribute1' => '0', 'attribute2' => '1'],
+            ['attribute1' => '2', 'attribute2' => '3'],
+        ]);
+
+        $this->assertEquals('<div class="ctable">' .
+            '<div class="ctheader">' .
+                '<div class="ctrow">' .
+                    '<div class="cthead">attribute1</div>' .
+                    '<div class="cthead">attribute2</div>' .
+                '</div>' .
+            '</div>' .
+            '<div class="ctrow">' .
+                '<div class="attribute1 ctcell">0</div>' .
+                '<div class="attribute2 ctcell">1</div>' .
+            '</div>' .
+            '<div class="ctrow">' .
+                '<div class="attribute1 ctcell">2</div>' .
+                '<div class="attribute2 ctcell">3</div>' .
+            '</div>' .
+        '</div>', $table->__toString());
+    }
+    // }}}
+    // {{{ testSimpleAttributes
+    public function testSimpleAttributes()
+    {
+        $table = new Table($this->page, [
+            ['attribute1' => '0', 'attribute2' => '1'],
+            ['attribute1' => '2', 'attribute2' => '3'],
+        ],
+        [
+            'attribute1' => 'Attribute I',
+            'attribute2' => 'Attribute II',
+        ]);
+
+        $this->assertEquals('<div class="ctable">' .
+            '<div class="ctheader">' .
+                '<div class="ctrow">' .
+                    '<div class="cthead">Attribute I</div>' .
+                    '<div class="cthead">Attribute II</div>' .
+                '</div>' .
+            '</div>' .
+            '<div class="ctrow">' .
+                '<div class="Attribute_I ctcell">0</div>' .
+                '<div class="Attribute_II ctcell">1</div>' .
+            '</div>' .
+            '<div class="ctrow">' .
+                '<div class="Attribute_I ctcell">2</div>' .
+                '<div class="Attribute_II ctcell">3</div>' .
+            '</div>' .
+        '</div>', $table->__toString());
+    }
+    // }}}
+}
