fc730d1151fd1344fb682dd618edf725ee0e1a2e
Sebastian Reinhold <sebastian@bitbernd.de>
2020-03-22 21:01:32 +0100

    add default editor for image text entities

--- CMEsteban/Page/Module/Form/EditImageTextEntity.php
+++ CMEsteban/Page/Module/Form/EditImageTextEntity.php
@@ -2,20 +2,20 @@
 
 namespace CMEsteban\Page\Module\Form;
 
-abstract class EditGalleryItem extends EditImageTextEntity
+abstract class EditGalleryItem extends EditImageEntity
 {
     protected function create()
     {
         parent::create();
 
-        $this->form->addNumber('Position');
+        $this->form->addTextarea('Text');
     }
     protected function populate()
     {
         parent::populate();
 
         $values = [
-            'Position' => $this->entity->getPosition(),
+            'Text' => $this->entity->getText(),
         ];
 
         $this->form->populate($values);
@@ -24,7 +24,7 @@ abstract class EditGalleryItem extends EditImageTextEntity
     {
         $values = $this->form->getValues();
 
-        $this->entity->setPosition($values['Position']);
+        $this->entity->setText($values['Text']);
 
         parent::save();
     }
