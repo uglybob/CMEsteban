65cd37ee627c10d335000cec3cf0620d09fbabfb
Sebastian Reinhold <sebastian@bitbernd.de>
2020-04-10 20:12:52 +0200

    adapt cleanText tests to new text module

--- CMEsteban/Test/CleanLinksTest.php
+++ CMEsteban/Test/CleanLinksTest.php
@@ -2,7 +2,7 @@
 
 namespace CMEsteban\Test;
 
-use CMEsteban\Page\Page;
+use CMEsteban\Page\Module\Text;
 
 class CleanLinksTest extends CMEstebanTestCase
 {
@@ -10,79 +10,79 @@ class CleanLinksTest extends CMEstebanTestCase
     {
         $testString = '';
 
-        $this->assertEquals($testString, Page::cleanText($testString));
+        $this->assertEquals($testString, Text::cleanText($testString));
     }
     public function testNoLink()
     {
         $testString = 'nolink';
 
-        $this->assertEquals($testString, Page::cleanText($testString));
+        $this->assertEquals($testString, Text::cleanText($testString));
     }
     public function testUrl()
     {
-        $this->assertEquals('<a href="https://www.url.com">>url.com</a>', Page::cleanText('www.url.com'));
+        $this->assertEquals('<a href="https://www.url.com">>url.com</a>', Text::cleanText('www.url.com'));
     }
     public function testUrlHttp()
     {
-        $this->assertEquals('<a href="http://www.url.com">>url.com</a>', Page::cleanText('http://www.url.com'));
+        $this->assertEquals('<a href="http://www.url.com">>url.com</a>', Text::cleanText('http://www.url.com'));
     }
     public function testUrlHttps()
     {
-        $this->assertEquals('<a href="https://www.url.com">>url.com</a>', Page::cleanText('https://www.url.com'));
+        $this->assertEquals('<a href="https://www.url.com">>url.com</a>', Text::cleanText('https://www.url.com'));
     }
     public function testUrlMultiple()
     {
-        $this->assertEquals('<a href="https://www.url.com">>url.com</a> <a href="https://www.url2.com">>url2.com</a>', Page::cleanText('www.url.com www.url2.com'));
+        $this->assertEquals('<a href="https://www.url.com">>url.com</a> <a href="https://www.url2.com">>url2.com</a>', Text::cleanText('www.url.com www.url2.com'));
     }
     public function testUrlRecursive()
     {
-        $this->assertEquals('<a href="https://www.url.com">>url.com</a> <a href="https://www.url.co">>url.co</a>', Page::cleanText('www.url.com www.url.co'));
+        $this->assertEquals('<a href="https://www.url.com">>url.com</a> <a href="https://www.url.co">>url.co</a>', Text::cleanText('www.url.com www.url.co'));
     }
     public function testUrlEnclosed()
     {
-        $this->assertEquals('|<a href="https://www.url.com">>url.com</a>|', Page::cleanText('|www.url.com|'));
+        $this->assertEquals('|<a href="https://www.url.com">>url.com</a>|', Text::cleanText('|www.url.com|'));
     }
     public function testUrlShortenKnown()
     {
-        $this->assertEquals('<a href="https://www.twitter.com/account">>twitter</a>', Page::cleanText('www.twitter.com/account'));
+        $this->assertEquals('<a href="https://www.twitter.com/account">>twitter</a>', Text::cleanText('www.twitter.com/account'));
     }
     public function testUrlShortenKnownInParams()
     {
-        $this->assertEquals('<a href="https://www.url.com/twitter">>url.com/twitter</a>', Page::cleanText('www.url.com/twitter'));
+        $this->assertEquals('<a href="https://www.url.com/twitter">>url.com/twitter</a>', Text::cleanText('www.url.com/twitter'));
     }
     public function testUrlShortenUgly()
     {
-        $this->assertEquals('<a href="https://encrypted.google.com/search?hl=en&q=php%20url%20info">>encrypted.google.com/search?hl...</a>', Page::cleanText('https://encrypted.google.com/search?hl=en&q=php%20url%20info'));
+        $this->assertEquals('<a href="https://encrypted.google.com/search?hl=en&q=php%20url%20info">>encrypted.google.com/search?hl...</a>', Text::cleanText('https://encrypted.google.com/search?hl=en&q=php%20url%20info'));
     }
     public function testUrlShortenTrailingSlash()
     {
-        $this->assertEquals('<a href="http://www.url.com/">>url.com</a>', Page::cleanText('http://www.url.com/'));
+        $this->assertEquals('<a href="http://www.url.com/">>url.com</a>', Text::cleanText('http://www.url.com/'));
     }
 
     public function testEmail()
     {
-        $this->assertEquals('<span class="shooo">nppbhag+ubfg,pbz</span>', Page::cleanText('account@host.com'));
+        $this->assertEquals('<span class="shooo">nppbhag+ubfg,pbz</span>', Text::cleanText('account@host.com'));
     }
     public function testEmailMultiple()
     {
-        $this->assertEquals('<span class="shooo">nppbhag+ubfg,pbz</span> <span class="shooo">nppbhag2+ubfg,pbz</span>', Page::cleanText('account@host.com account2@host.com'));
+        $this->assertEquals('<span class="shooo">nppbhag+ubfg,pbz</span> <span class="shooo">nppbhag2+ubfg,pbz</span>', Text::cleanText('account@host.com account2@host.com'));
     }
     public function testEmailRecursive()
     {
-        $this->assertEquals('<span class="shooo">nppbhag+ubfg,pbz</span> <span class="shooo">ppbhag+ubfg,pbz</span>', Page::cleanText('account@host.com ccount@host.com'));
+        $this->assertEquals('<span class="shooo">nppbhag+ubfg,pbz</span> <span class="shooo">ppbhag+ubfg,pbz</span>', Text::cleanText('account@host.com ccount@host.com'));
     }
     public function testEnclosed()
     {
-        $this->assertEquals('|<span class="shooo">nppbhag+ubfg,pbz</span>|', Page::cleanText('|account@host.com|'));
+        $this->assertEquals('|<span class="shooo">nppbhag+ubfg,pbz</span>|', Text::cleanText('|account@host.com|'));
     }
 
     public function testMixed()
     {
-        $this->assertEquals('<span class="shooo">nppbhag+ubfg,pbz</span> <a href="https://www.url.com">>url.com</a>', Page::cleanText('account@host.com www.url.com'));
+        $this->assertEquals('<span class="shooo">nppbhag+ubfg,pbz</span> <a href="https://www.url.com">>url.com</a>', Text::cleanText('account@host.com www.url.com'));
     }
 
     public function testLineBreak()
     {
-        $this->assertEquals('<br />', Page::cleanText("\n"));
+        $this->assertEquals('<br />', Text::cleanText("\n"));
     }
 }
